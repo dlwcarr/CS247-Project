4962c7b2ffaf2c2293237cb34c42a05056cb5696
alexlaviolette <alex.laviolette92@gmail.com>
2012-06-20 16:32:24 -0400

    Added extra case for first human player

--- Game.cpp
+++ Game.cpp
@@ -77,7 +77,7 @@ void Game::start() {
 			vector<Card> validPlays;
 			validPlays.push_back(Card(SPADE, SEVEN));
 
-            if(human[index])
+            if(human[startingPlayer])
 					printTable();
 			// Get command from first player
 			Command cmd = players_[startingPlayer]->getCommand(validPlays);
