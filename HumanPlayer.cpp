81e91f8f6bbf130f7466557a105b435482ecb02f
Dane Carr <dane.l.w.carr@gmail.com>
2012-06-19 21:40:15 -0400

    Implemented HumanPlayer getCommand and play functions

--- HumanPlayer.cpp
+++ HumanPlayer.cpp
@@ -12,15 +12,39 @@ HumanPlayer::~HumanPlayer() {
 }
 
 Command HumanPlayer::getCommand(vector<Card*>& legalPlays) const {
-	cout << "Legal plays:";
-	for (int i = 0; i < legalPlays.size(); i++) {
-		cout << legalPlays[i];
-		if (i != legalPlays.size() - 1)
-			cout << " ";
-	}
+	vector<Card*> hand = getHand();
+	cout << "Your hand:";
+	for (int i = 0; i < hand.size(); i++)
+		cout << " " << hand[i];
+	cout << endl << "Legal plays:";
+	for (int i = 0; i < legalPlays.size(); i++)
+		cout << " " << legalPlays[i];
 	cout << endl;
+
+	Command cmd;
+	bool flag = true;
+
+	while (flag) {
+		cout << ">";
+		cin >> cmd;
+		if (cmd.type == PLAY) {
+			for (int i = 0; i < legalPlays->size(); i++) {
+				if (cmd.card == *legalPlays[i])
+					flag = false;
+			}
+		}
+		else 
+			flag = false;
+	}
+
+	return cmd;
 }
 
 void HumanPlayer::play(Command c) {
-
+	if (c.type == PLAY) {
+		removeHand(c.card);
+	}
+	else if (c.type == DISCARD) {
+		discard(c.card);
+	}
 }
