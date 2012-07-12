2ee895975b057652c1d9c2efbd4abc5cf68b3a64
alexlaviolette <alex.laviolette92@gmail.com>
2012-07-11 20:09:09 -0400

    forgot a deref

--- GUI.cpp
+++ GUI.cpp
@@ -169,7 +169,7 @@ tableSpadeHBox(true, 5), playersHBox(true, 1), handHBox(true, 5) {
 		for(int i = 0; i < 13; i ++) {
 			handHBox.add( *handCards[i] );
 				handImages[i] = new Gtk::Image( nullCardPixbuf );
-				handCards[i]->set_image(handImages[i]);
+				handCards[i]->set_image( *handImages[i] );
 		}
 
 
